68560629767fe9639cc6cebe91c630f36b62bd2a
1580923067 <1580923067@qq.com>
2020-12-08 00:58:18 +0800

    刷题

--- src/leetcode/LinkedList/problem024_两两交换链表中的节点.java
+++ src/leetcode/LinkedList/problem024_两两交换链表中的节点.java
@@ -1,6 +1,39 @@
 package leetcode.LinkedList;
 
+import leetcode.Structure.ListNode;
+
 public class problem024_两两交换链表中的节点 {
 
 
+    public static class problem206_反转链表 {
+        /**
+         * 迭代
+         */
+        public ListNode reverseList(ListNode head) {
+            ListNode prev = null;
+            ListNode curr = head;
+            while (curr != null) {
+                ListNode nextTemp = curr.next;
+                curr.next = prev;
+                prev = curr;
+                curr = nextTemp;
+            }
+            return prev;
+        }
+
+        /**
+         * 递归
+         * @param head
+         * @return
+         */
+        public ListNode reverseList2(ListNode head) {
+            if (head==null||head.next==null){
+                return head;
+            }
+            ListNode p=reverseList2(head.next);
+            head.next.next=head;
+            head.next=null;
+            return p;
+        }
+    }
 }
