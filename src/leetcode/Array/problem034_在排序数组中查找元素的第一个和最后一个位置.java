8176476615a649092fa18522d3c07c310bf72169
1580923067 <1580923067@qq.com>
2020-12-16 22:13:16 +0800

    12.11 update 动态规划专题

--- src/leetcode/Array/problem034_在排序数组中查找元素的第一个和最后一个位置.java
+++ src/leetcode/Array/problem034_在排序数组中查找元素的第一个和最后一个位置.java
@@ -1,5 +1,27 @@
 package leetcode.Array;
 
 public class problem034_在排序数组中查找元素的第一个和最后一个位置 {
+    public int[] searchRange(int[] nums, int target) {
+        int leftInx = binarySearch(nums, target, true);
+        int rightInx = binarySearch(nums, target, false) - 1;
 
+        if (leftInx <= rightInx && rightInx <= nums.length && nums[leftInx] == target && nums[rightInx] == target) {
+            return new int[]{leftInx, rightInx};
+        }
+        return new int[]{-1, -1};
+    }
+
+    public int binarySearch(int[] nums, int target, boolean lower) {
+        int left = 0, right = nums.length - 1, ans = nums.length;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] > target || (lower && nums[mid] >= target)) {
+                right = mid - 1;
+                ans = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return ans;
+    }
 }
---
+++ src/leetcode/String/problem003_无重复字符的最长子串.java
@@ -0,0 +1,38 @@
+package leetcode.String;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class problem003_无重复字符的最长子串 {
+    public int lengthOfLongestSubstring(String s) {
+        Set<Character> occ = new HashSet<Character>();
+        int n = s.length();
+        int rk = -1, ans = 0;
+        for (int i = 0; i < n; ++i) {
+            if (i != 0) {
+                occ.remove(s.charAt(i - 1));
+            }
+            while (rk + 1 < n && !occ.contains(s.charAt(rk + 1))) {
+                occ.add(s.charAt(rk + 1));
+                ++rk;
+            }
+            ans = Math.max(ans, rk - i + 1);
+        }
+        return ans;
+    }
+
+    public int lengthOfLongestSubstring2(String s) {
+        int n = s.length(), ans = 0;
+        Map<Character, Integer> map = new HashMap<>();
+        for (int end = 0, start = 0; end < n; end++) {
+            char alpha = s.charAt(end);
+            if (map.containsKey(alpha)) {
+                start = Math.max(ans, end - start + 1);
+                map.put(s.charAt(end), end + 1);
+            }
+        }
+        return ans;
+    }
+}
