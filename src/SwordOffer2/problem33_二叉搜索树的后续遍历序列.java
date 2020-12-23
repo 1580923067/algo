d5f18834d6fc0480bfa62c974b3238afce96bcb4
1580923067 <1580923067@qq.com>
2020-12-23 13:11:29 +0800

    12.23 update 剑指offer

---
+++ src/SwordOffer2/problem33_二叉搜索树的后续遍历序列.java
@@ -0,0 +1,32 @@
+package SwordOffer2;
+
+import java.util.Stack;
+
+public class problem33_二叉搜索树的后续遍历序列 {
+    public boolean verifyPostorder(int[] postorder) {
+        return recur(postorder, 0, postorder.length - 1);
+    }
+
+    public boolean recur(int[] postorder, int i, int j) {
+        if (i > j) return true;
+        int p = i;
+        while (postorder[p] > postorder[j]) p++;
+        int m = p;
+        while (postorder[p] > postorder[j]) p++;
+        return p == j && recur(postorder, i, m - 1) && recur(postorder, m, j - 1);
+
+    }
+
+    public boolean verifyPostorder2(int[] postorder) {
+        Stack<Integer> stack = new Stack<>();
+        int root = Integer.MAX_VALUE;
+        for (int i = postorder.length - 1; i >= 0; i--) {
+            if (postorder[i] > root) return false;
+            while (!stack.isEmpty() && stack.peek() > postorder[i])
+                root = stack.pop();
+            stack.add(postorder[i]);
+        }
+        return true;
+    }
+
+}
