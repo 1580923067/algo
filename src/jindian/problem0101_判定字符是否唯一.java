54a8bb0636774022c10a585e8a039b506427f48b
1580923067 <1580923067@qq.com>
2020-12-26 17:36:36 +0800

    12.26 update 面试金典

--- src/jindian/problem0101_判定字符是否唯一.java
+++ src/jindian/problem0101_判定字符是否唯一.java
@@ -1,5 +1,17 @@
 package jindian;
 
 public class problem0101_判定字符是否唯一 {
-
+    public boolean kisUnique(String astr) {
+        long bits = 0;
+        int size = astr.length();
+        for (int i = 0; i < size; i++) {
+            int move = astr.charAt(i) - 'A';
+            if ((bits & (1 << move)) != 0) {
+                return false;
+            } else {
+                bits |= (1 << move);
+            }
+        }
+        return true;
+    }
 }
