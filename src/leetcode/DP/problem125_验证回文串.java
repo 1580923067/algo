7688873b21baea07dbc1fca9a43a58555517d7fd
1580923067 <1580923067@qq.com>
2021-01-05 22:28:10 +0800

    2021.01.05 update

--- src/leetcode/DP/problem125_验证回文串.java
+++ src/leetcode/DP/problem125_验证回文串.java
@@ -34,4 +34,25 @@ public class problem125_验证回文串 {
         }
         return true;
     }
+
+    public boolean isPalindrome3(String s) {
+        int n = s.length();
+        int left = 0, right = n - 1;
+        while (left < right) {
+            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+                ++left;
+            }
+            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+                --right;
+            }
+            if (left < right) {
+                if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
+                    return false;
+                }
+                ++left;
+                --right;
+            }
+        }
+        return true;
+    }
 }
