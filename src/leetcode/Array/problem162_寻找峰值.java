8b2cd2ecbe0584d924a3cce1b8b810ef0275b246
1580923067 <1580923067@qq.com>
2021-01-06 21:00:56 +0800

    2021.01.06 update

--- src/leetcode/Array/problem162_寻找峰值.java
+++ src/leetcode/Array/problem162_寻找峰值.java
@@ -1,4 +1,11 @@
 package leetcode.Array;
 
 public class problem162_寻找峰值 {
+    public int findPeakElement(int[] nums){
+        for (int i = 0; i < nums.length-1; i++) {
+            if (nums[i]>nums[i+1])
+                return i;
+        }
+        return nums.length-1;
+    }
 }
---
+++ src/leetcode/Array/problem217_存在重复元素.java
@@ -0,0 +1,15 @@
+package leetcode.Array;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class problem217_存在重复元素 {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (!set.add(num))
+                return true;
+        }
+        return false;
+    }
+}
