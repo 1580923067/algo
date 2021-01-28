4c671a0d9ebc54bf5fc9eb95d2a19f1b0d77fad0
1580923067 <1580923067@qq.com>
2021-01-29 01:39:22 +0800

    0129

---
+++ src/leetcode/Tree/problem230_二叉搜索树中第K小的元素.java
@@ -0,0 +1,20 @@
+package leetcode.Tree;
+
+import leetcode.Structure.TreeNode;
+
+import java.util.ArrayList;
+
+public class problem230_二叉搜索树中第K小的元素 {
+    public int kthSmallest(TreeNode root, int k) {
+        ArrayList<Integer> nums = inorder(root, new ArrayList<Integer>());
+        return nums.get(k - 1);
+    }
+
+    public ArrayList<Integer> inorder(TreeNode root, ArrayList<Integer> arr) {
+        if (root == null) return arr;
+        inorder(root.left, arr);
+        arr.add(root.val);
+        inorder(root.right, arr);
+        return arr;
+    }
+}
