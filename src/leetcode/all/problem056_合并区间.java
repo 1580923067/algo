e6aa196a425e5835f5a99e10f95742b5ccb85b5e
1580923067 <1580923067@qq.com>
2022-03-16 22:27:14 +0800

    add 03-16

--- src/leetcode/all/problem056_合并区间.java
+++ src/leetcode/all/problem056_合并区间.java
@@ -18,15 +18,29 @@ public class problem056_合并区间 {
         });
         List<int[]> merged = new ArrayList<>();
         for (int i = 0; i < intervals.length; ++i) {
-            int L = intervals[i][0], R = intervals[i][0];
+            int L = intervals[i][0], R = intervals[i][1];
             // merged数组为空，或者merged数组最右端小于新数组的左端值，则将新数组加入merged
             if (merged.size() == 0 || merged.get(merged.size() - 1)[1] < L) {
                 merged.add(new int[]{L, R});
-            // 否则，更新数组的右端值
+                // 否则，更新数组的右端值
             } else {
                 merged.get(merged.size() - 1)[1] = Math.max(merged.get(merged.size() - 1)[1], R);
             }
         }
         return merged.toArray(new int[merged.size()][]);
+    }
+    // https://mp.weixin.qq.com/s/ioUlNa4ZToCrun3qb4y4Ow
+    public int[][] merge2(int[][] intervals) {
+        Arrays.sort(intervals, (v1, v2) -> v1[0] - v2[0]);
+        int[][] res = new int[intervals.length][2];
+        int idx = -1;
+        for (int[] interval : intervals) {
+            if (idx == -1 || interval[0] > res[idx][1]) {
+                res[++idx] = interval;
+            } else {
+                res[idx][1] = Math.max(res[idx][1], interval[1]);
+            }
+        }
+        return Arrays.copyOf(res, idx + 1);
     }
 }
--- src/leetcode/all/problem300_最长递增子序列.java
+++ src/leetcode/all/problem300_最长递增子序列.java
@@ -1,5 +1,28 @@
 package leetcode.all;
 
 public class problem300_最长递增子序列 {
+    public int lengthOfLIS(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        int[] dp = new int[nums.length];
+        dp[0] = 1;
+        int maxans = 1;
+        for (int i = 1; i < nums.length; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            maxans = Math.max(maxans, dp[i]);
+        }
+        return maxans;
+    }
 
+    public static void main(String[] args) {
+        problem300_最长递增子序列 solution = new problem300_最长递增子序列();
+        int[] nums = {10, 9, 2, 5, 3, 7, 101, 18};
+        System.out.println(solution.lengthOfLIS(nums));
+    }
 }
