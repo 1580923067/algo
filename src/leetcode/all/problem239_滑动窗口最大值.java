07f7e7a30081337b7905e69a996f0185ce9203c0
1580923067 <1580923067@qq.com>
2022-03-17 09:30:11 +0800

    add 03-17

--- src/leetcode/all/problem239_滑动窗口最大值.java
+++ src/leetcode/all/problem239_滑动窗口最大值.java
@@ -1,5 +1,6 @@
 package leetcode.all;
 
+import java.util.Arrays;
 import java.util.Deque;
 import java.util.LinkedList;
 
@@ -8,14 +9,14 @@ public class problem239_滑动窗口最大值 {
     public int[] maxSlidingWindow(int[] nums, int k) {
         int n = nums.length;
         Deque<Integer> deque = new LinkedList<>();
-        for (int i = 0; i < k; i++) {
+        for (int i = 0; i < k; ++i) {
             while (!deque.isEmpty() && nums[i] > deque.peekLast()) {
                 deque.pollLast();
             }
             deque.offerLast(i);
         }
 
-        int[] ans = new int[n - k - 1];
+        int[] ans = new int[n - k + 1];
         ans[0] = nums[deque.peekFirst()];
         for (int i = k; i < n; ++i) {
             while (!deque.isEmpty() && nums[i] >= nums[deque.peekLast()]) {
@@ -28,6 +29,15 @@ public class problem239_滑动窗口最大值 {
             ans[i - k + 1] = nums[deque.peekFirst()];
         }
         return ans;
+    }
+
+    public static void main(String[] args) {
+        problem239_滑动窗口最大值 solution = new problem239_滑动窗口最大值();
+        int[] nums = {1, 3, -1, -3, 5, 3, 6, 7};
+        int k = 3;
+        int[] ans = solution.maxSlidingWindow(nums, k);
+        System.out.println(Arrays.toString(ans));
+
     }
 
 }
