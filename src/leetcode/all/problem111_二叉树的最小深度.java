f6e9d24e30d8a9688c9389172677b33d7a38139c
1580923067 <1580923067@qq.com>
2022-04-01 09:18:02 +0800

    add 04-01

--- src/leetcode/all/problem111_二叉树的最小深度.java
+++ src/leetcode/all/problem111_二叉树的最小深度.java
@@ -2,6 +2,9 @@ package leetcode.all;
 
 import leetcode.Structure.TreeNode;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class problem111_二叉树的最小深度 {
     public int minDepth(TreeNode root) {
         if (root == null) {
@@ -20,5 +23,54 @@ public class problem111_二叉树的最小深度 {
         return min_depth + 1;
     }
 
+    class QueueNode {
 
+        TreeNode node;
+        int depth;
+
+        public QueueNode(TreeNode node, int depth) {
+            this.node = node;
+            this.depth = depth;
+        }
+
+    }
+
+    public int minDepth1(TreeNode root) {
+        if (root == null) {
+            return 0;
+
+        }
+        Queue<QueueNode> queue = new LinkedList<>();
+        queue.offer(new QueueNode(root, 1));
+        while (!queue.isEmpty()) {
+            QueueNode nodeDepth = queue.poll();
+            TreeNode node = nodeDepth.node;
+            int depth = nodeDepth.depth;
+            if (node.left == null && node.right == null) {
+                return depth;
+            }
+            if (node.left != null) {
+                queue.offer(new QueueNode(node.left, depth + 1));
+            }
+            if (node.right != null) {
+                queue.offer(new QueueNode(node.right, depth + 1));
+            }
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        problem111_二叉树的最小深度 solution = new problem111_二叉树的最小深度();
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+        root.right = new TreeNode(20);
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
+        int res = solution.minDepth(root);
+        System.out.println(res);
+        int res1 = solution.minDepth1(root);
+        System.out.println(res1);
+    }
 }
